c4e19371a246ce5f76e65b41e218a41d0980f760
xmarkas <xmarkas@hotmail.com>
2025-03-05 08:42:40 -0600

    barcode ouput

--- src/components/BarcodeC.tsx
+++ src/components/BarcodeC.tsx
@@ -1,37 +1,53 @@
-import { useState } from 'react'
-import { BarcodeScanner, DetectedBarcode, ScanOptions, BarcodeFormat} from 'react-barcode-scanner'
-import "react-barcode-scanner/polyfill"
+import { useState } from "react";
+import {
+  BarcodeScanner,
+  DetectedBarcode,
+  ScanOptions,
+  BarcodeFormat,
+} from "react-barcode-scanner";
+import "react-barcode-scanner/polyfill";
 
-function BarcodeC({onResult = () => {}}:any) { 
-      const [result, setResult] = useState<string | undefined>("");
+function BarcodeC({ onResult = () => {} }: any) {
+  const [result, setResult] = useState<DetectedBarcode[]>([]);
 
   const handleCapture = (barcode: DetectedBarcode[]) => {
-      setResult(barcode.entries().next().value?.[1].rawValue)
-      onResult(barcode.values().next().value);
-  }
-
-
+    const code = barcode;
+    setResult(code);
+    onResult(barcode.values().next().value);
+  };
 
   const constraintsConfig: MediaTrackConstraints = {
     facingMode: {
-      ideal: 'environment'
+      ideal: "environment",
     },
-    
-  }
-  const formats = [BarcodeFormat.CODE_128, BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39]
+  };
+  const formats = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.QR_CODE,
+    BarcodeFormat.CODE_39,
+  ];
   const optionsConfig: ScanOptions = {
     delay: 500,
     formats: formats,
-  }
+  };
 
   return (
     <>
       <div>
-        <BarcodeScanner options={optionsConfig} trackConstraints={constraintsConfig} onCapture={handleCapture} />
-        <div>{result}</div>
-       </div>
+        <BarcodeScanner
+          options={optionsConfig}
+          trackConstraints={constraintsConfig}
+          onCapture={handleCapture}
+        />
+        {result.map((e) => (
+          <div>
+            <div>{e.format}</div>
+            <div>{e.rawValue}</div>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default BarcodeC
+export default BarcodeC;
