06de9b10c245aea2d9094bbbd5be61cfb45151e5
xmarkas <xmarkas@hotmail.com>
2025-03-05 09:24:23 -0600

    barcode bounding box

--- src/components/BarcodeC.tsx
+++ src/components/BarcodeC.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import {
   BarcodeScanner,
@@ -8,22 +7,28 @@ import {
 } from "react-barcode-scanner";
 import "react-barcode-scanner/polyfill";
 
-function BarcodeC({ onResult = () => {} }: any) {
+function BarcodeC() {
   const [value, setValue] = useState("");
   const [format, setFormat] = useState("");
-  const [styles, setStyles] = useState({top: 100, left: 100, height: 200, width: 100});
+  const [styles, setStyles] = useState({
+    top: 100,
+    left: 100,
+    height: 200,
+    width: 100,
+  });
 
   const handleCapture = (barcode: DetectedBarcode[]) => {
     const code = barcode[0];
     setValue(code.rawValue);
     setFormat(code.format);
 
-
-    onResult(code.rawValue);
-    
     const bBox = code.boundingBox;
-    setStyles({ top: bBox.top, left: bBox.left, height: bBox.height, width: bBox.width})
-
+    setStyles({
+      top: bBox?.top | 10,
+      left: bBox?.left | 10,
+      height: bBox?.height | 10,
+      width: bBox?.width | 10,
+    });
   };
 
   const constraintsConfig: MediaTrackConstraints = {
@@ -48,14 +53,15 @@ function BarcodeC({ onResult = () => {} }: any) {
           options={optionsConfig}
           trackConstraints={constraintsConfig}
           onCapture={handleCapture}
-          
         />
-          <div id="capture" style={{...styles, position: "absolute", border: "1px solid red"}}></div>
-          <div>
-            <div>{format}</div>
-            <div>{value}</div>
-          </div>
-        
+        <div
+          id="capture"
+          style={{ ...styles, position: "absolute", border: "1px solid red" }}
+        ></div>
+        <div>
+          <div>{format}</div>
+          <div>{value}</div>
+        </div>
       </div>
     </>
   );
