1a0c34337dd653f17bf663790d4c83a0c5ad3a88
xmarkas <xmarkas@hotmail.com>
2025-03-11 19:14:16 -0500

    BRC updates

--- src/components/BCR.tsx
+++ src/components/BCR.tsx
@@ -1,46 +1,17 @@
 import { Grid2 } from "@mui/material";
-import { useState } from "react";
-// import { useMediaDevices } from "react-media-devices";
-import { useZxing } from "react-zxing";
 
-const constraints: MediaStreamConstraints = {
-  video: {
-    facingMode: "environment",
-    height: 500,
-    width: 500
-  },
-  audio: false,
-  
-};
+
+
 
 export const BRC = () => {
-  const [result, setResult] = useState("");
-  //   const { devices } = useMediaDevices({ constraints });
-  //   const deviceId = devices?.[0]?.deviceId;
-
-  const handleCapture = (result: any) => {
-    setResult(result.text);
-  };
-
-  const { ref } = useZxing({
-    
-    onResult: (r: any) => handleCapture(r),
-    constraints: constraints,
-    
-  });
-
-  // useEffect(() => {
-  //   navigator.mediaDevices.getUserMedia({ video: true }).then((p) => {
-  //     setResult(`${p.active} `);
-  //   });
-  // }, []);
+ 
 
   return (
     <Grid2 container={true}>
       <Grid2 size={{xs:12}} style={{border: "1px solid red", height: "100%", width: "100%"}}>
-      <video ref={ref}  style={{zIndex:1000}}playsInline={true}
+      <video   style={{zIndex:1000}} playsInline={true}
       />
-      <span>{result} - {process.env.gid}</span>
+      <span> {import.meta.env.VITE_GID}</span>
       </Grid2>
       
     </Grid2>
