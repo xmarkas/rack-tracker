1531ddcb5a32fa9e920e2d3a542bb66235765797
xmarkas <xmarkas@hotmail.com>
2025-03-11 19:42:12 -0500

    BRC updates

--- src/components/BCR.tsx
+++ src/components/BCR.tsx
@@ -1,7 +1,7 @@
 import { Grid2 } from "@mui/material";
 
 const gid = process.env.VITE_GID
-
+const blah = import.meta.env.VITE_GID
 
 export const BCR = () => {
  console.log("-->", gid)
@@ -11,7 +11,7 @@ export const BCR = () => {
       <Grid2 size={{xs:12}} style={{border: "1px solid red", height: "100%", width: "100%"}}>
       <video   style={{zIndex:1000}} playsInline={true}
       />
-      <span>nope {gid}</span>
+      <span>nope {gid}{blah}</span>
       </Grid2>
       
     </Grid2>
