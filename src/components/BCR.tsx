95eb7d262100f64f1cd7fd3a6d1b34036e416311
xmarkas <xmarkas@hotmail.com>
2025-03-11 19:49:13 -0500

    BRC updates

--- src/components/BCR.tsx
+++ src/components/BCR.tsx
@@ -1,17 +1,16 @@
 import { Grid2 } from "@mui/material";
 
-const gid = process.env.VITE_GID
-const blah = import.meta.env.VITE_GID
+
 
 export const BCR = () => {
- console.log("-->", gid)
+ 
 
   return (
     <Grid2 container={true}>
       <Grid2 size={{xs:12}} style={{border: "1px solid red", height: "100%", width: "100%"}}>
       <video   style={{zIndex:1000}} playsInline={true}
       />
-      <span>nope {gid}{blah}</span>
+      <span></span>
       </Grid2>
       
     </Grid2>
--- src/tools/sheetsLink.ts
+++ src/tools/sheetsLink.ts
@@ -1,11 +1,9 @@
 const sheetLink = "https://docs.google.com/spreadsheets/d/1-C6l615Fx8oP_X0VFjTiy6-2txplF8AteGZZIfuTbzY/export?format=csv&gid=";
-//                     https://docs.google.com/spreadsheets/d/1-C6l615Fx8oP_X0VFjTiy6-2txplF8AteGZZIfuTbzY/edit?usp=sharing
-
-// "https://docs.google.com/spreadsheets/d/1-C6l615Fx8oP_X0VFjTiy6-2txplF8AteGZZIfuTbzY/edit?gid=939283605#gid=939283605"
+const gid = import.meta.env.VITE_GID
 
 export const sheetsLink = () => {
-  const gid = "1201259274";
   const csvDownload = `${sheetLink}${gid}`;
   
   return csvDownload;
 };
+
