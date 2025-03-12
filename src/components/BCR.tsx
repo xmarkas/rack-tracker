14b03c42cabb6e580b75bbc92e9e683fab1a6abb
xmarkas <xmarkas@hotmail.com>
2025-03-11 21:48:53 -0500

    BRC updates

--- src/components/BCR.tsx
+++ src/components/BCR.tsx
@@ -1,5 +1,5 @@
 import { Grid2 } from "@mui/material";
-import {  useState } from "react";
+import { useState } from "react";
 // import { useMediaDevices } from "react-media-devices";
 import { useZxing } from "react-zxing";
 
@@ -7,10 +7,9 @@ const constraints: MediaStreamConstraints = {
   video: {
     facingMode: "environment",
     height: 500,
-    width: 500
+    width: 500,
   },
   audio: false,
-  
 };
 
 export const BCR = () => {
@@ -26,18 +25,60 @@ export const BCR = () => {
     paused: false,
     onResult: (r: any) => handleCapture(r),
     constraints: constraints,
-    
   });
 
-
-
   return (
-    <Grid2 container>
-      <Grid2 size={{xs:12}} style={{border: "1px solid red", height: "100%", width: "100%"}}>
-      <video ref={ref}  />
-      <span>{result}</span>
+    <Grid2 container sx={{ background: "black", height: "75vh" }}>
+      <Grid2
+        size={{ xs: 12 }}
+        position={"absolute"}
+        display={"flex"}
+        width={"100vw"}
+        height={"75vh"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flex={1}
+        top={0}
+        left={0}
+      >
+        <div
+          style={{
+            background: "lightblue",
+            position: "absolute",
+            left: "calc(50vw - 100px)",
+            top: "calc(37vh - 100px)"
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              borderRight: "1px solid red",
+              height: "200px",
+              width: "100px",
+              zIndex: 100,
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              borderBottom: "1px solid red",
+              height: "100px",
+              width: "200px",
+              zIndex: 100,
+            }}
+          ></div>
+        </div>
+        <video ref={ref} style={{ backgroundSize: "contain" }} />
       </Grid2>
-      
+      <Grid2
+        size={{ xs: 12 }}
+        textAlign={"center"}
+        sx={{ background: "black", color: "white" }}
+        display={"flex"}
+        flex={1}
+      >
+        {result}
+      </Grid2>
     </Grid2>
   );
 };
