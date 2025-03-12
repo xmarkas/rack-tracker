e7fc29f9c8402aa7cf9252450cdd1078c8b1d626
xmarkas <xmarkas@hotmail.com>
2025-03-11 20:17:02 -0500

    BRC updates

--- src/components/BCR.tsx
+++ src/components/BCR.tsx
@@ -1,16 +1,45 @@
 import { Grid2 } from "@mui/material";
+import { useEffect, useState } from "react";
+// import { useMediaDevices } from "react-media-devices";
+import { useZxing } from "react-zxing";
 
-
+const constraints: MediaStreamConstraints = {
+  video: {
+    facingMode: "environment",
+    height: 500,
+    width: 500
+  },
+  audio: false,
+  
+};
 
 export const BCR = () => {
- 
+  const [result, setResult] = useState("");
+  //   const { devices } = useMediaDevices({ constraints });
+  //   const deviceId = devices?.[0]?.deviceId;
+
+  const handleCapture = (result: any) => {
+    setResult(result.text);
+  };
+
+  const { ref } = useZxing({
+    paused: false,
+    onResult: (r: any) => handleCapture(r),
+    constraints: constraints,
+    
+  });
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((p) => {
+      setResult(`${p.active} `);
+    });
+  }, []);
 
   return (
-    <Grid2 container={true}>
+    <Grid2 container>
       <Grid2 size={{xs:12}} style={{border: "1px solid red", height: "100%", width: "100%"}}>
-      <video   style={{zIndex:1000}} playsInline={true}
-      />
-      <span></span>
+      <video ref={ref}  />
+      <span>{result}</span>
       </Grid2>
       
     </Grid2>
